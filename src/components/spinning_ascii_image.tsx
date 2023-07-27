8670d15ed206ae4a2017cd3793870c166dc78cea
simonyu <simonyu180@gmail.com>
2023-07-27 12:44:11 -0700

    Initial commit

---
+++ src/components/spinning_ascii_image.tsx
@@ -0,0 +1,12 @@
+
+
+const SpinnningAsciiImage = (props: { image: string }) => {
+    return (
+        <div className="flex justify-center items-center">
+            { props.image }
+        </div>
+
+    )
+}
+
+export default SpinnningAsciiImage;
