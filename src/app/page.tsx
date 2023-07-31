77104f6d58c40ceb3d23a475db3047e2015943b5
simonyu <simonyu180@gmail.com>
2023-07-31 15:52:18 -0700

    Connected front-end to back-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 import data from './api/frames.json'
 import SpinningAsciiImage from './components/spinning_ascii_image'
 
-export default function Home() {
+async function getFrames() {
+  const res = await fetch("https://spinning-ascii.shuttleapp.rs");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch frames");
+  }
+
+  return await res.json();
+}
+
+export default async function Home() {
+
+  const data = await getFrames();
+
   return (
     <main className="flex min-h-screen flex-col justify-center items-center">
       <SpinningAsciiImage frames={data.frames} />
