96e5565c082caea0a22a653e04f676b7e65a7f2d
simonyu <simonyu180@gmail.com>
2023-08-03 13:17:25 -0700

    Added custom image upload and display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,49 @@
-import data from './api/frames.json'
+'use client'
+import { useEffect, useState, useTransition } from 'react';
+
 import SpinningAsciiImage from './components/spinning_ascii_image'
+import UploadForm from './components/uploadForm';
+import RectButton from './components/rectButton';
+import json_data from './api/frames.json';
+import processImage from './action';
 
-async function getFrames() {
-  const res = await fetch("https://spinning-ascii.shuttleapp.rs");
+const Home = () => {
+  const [isPending, startTransition] = useTransition();
+  const [frames_data, setFramesData] = useState(json_data.frames);
+  const [formData, setFormData] = useState(new FormData(undefined));
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch frames");
+  useEffect(() => {
+    startTransition(() => {
+      processImage(formData).then(
+        (json) => { 
+          console.log("Returning fetched JSON!")
+          setFramesData(json.frames);
+        },
+        (reason) => { 
+          console.warn(reason) 
+          console.log("Returning default JSON!");
+        }
+      )
+    });
+  }, [formData]);
+
+  if (isPending) {
+    return (
+      <div className='h-screen flex justify-center items-center'>
+        Loading...
+      </div>
+    )
   }
 
-  return await res.json();
-}
-
-export default async function Home() {
-
-  const data = await getFrames();
-
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center">
-      <SpinningAsciiImage frames={data.frames} />
+    <main className="py-20 flex h-screen flex-col justify-center items-center">
+      {frames_data? <SpinningAsciiImage frames={frames_data}/> : <>Loading...</>}
+      <div className='grow'></div>
+      <UploadForm action={ (formData) => setFormData(formData) }>
+        <RectButton type="submit">Upload</RectButton>
+      </UploadForm>
     </main>
   )
 }
+
+export default Home;
