ffe5dd619e05914fb5ffa66eab2f5e8f12d0cfb0
simonyu <simonyu180@gmail.com>
2023-08-03 13:14:55 -0700

    Created server action for processing image

---
+++ src/app/action.tsx
@@ -0,0 +1,19 @@
+'use server'
+
+export default async function processImage(data: FormData) {
+
+  if (!data.has("image")) {
+    throw new Error("No image selected");
+  }
+
+  const res = await fetch("https://spinning-ascii.shuttleapp.rs/upload", { 
+    method: "POST",
+    body: data,
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch frames");
+  }
+
+  return await JSON.parse(await res.json());
+}
