5b4d9f9db119eb595f9514747ef0c4bb82941d64
simonyu <simonyu180@gmail.com>
2023-08-03 13:48:02 -0700

    Fixed spacing betweeen character on each row

--- src/app/components/spinning_ascii_image.tsx
+++ src/app/components/spinning_ascii_image.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const SpinnningAsciiImage = (props: { frames: {matrix: string[][]; frame_number:number;}[] }) => {
 
-    const fps = 30; // Frames per second
+    const fps = 20; // Frames per second
     const duration = 1000/fps; // Seconds per frame
     const [frameIndex, setFrameIndex] = useState(0);
 
@@ -20,10 +20,16 @@ const SpinnningAsciiImage = (props: { frames: {matrix: string[][]; frame_number:
     }, []);
 
     return (
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center">
             { props.frames[frameIndex].matrix.map((row, row_index) => {
                 return (
-                    <div key={frameIndex+row_index}>{ row }</div>
+                    <div key={row_index}> {
+                    row.map((char, char_index) => {
+                            return (
+                                <span key={char_index}>{char + " "}</span>
+                            )
+                        })
+                    }</div>
                 )
             })}
         </div>
